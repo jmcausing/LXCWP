b70bf0b2208d1d5e0538cedfa185bd5db9fc1197
vagrant <vagrant@vagrant.vm>
2020-08-10 14:06:29 +0000

    fix conf yml playbook

--- lxcgen-ansi.sh
+++ lxcgen-ansi.sh
@@ -233,9 +233,18 @@ then
 else
    echo "# Downloading nginx default file to files/default"
    wget https://raw.githubusercontent.com/jmcausing/LXCWP/master/files/default
-  
 fi
 
+
+if [[ $(ls vars.yml | grep vars.yml) ]]; 
+then
+  echo "# 'default' file exist. Deleting.. Then download a new one"
+      rm default*
+      wget https://raw.githubusercontent.com/jmcausing/LXCWP/master/files/default
+else
+   echo "# Downloading nginx default file to files/default"
+   wget https://raw.githubusercontent.com/jmcausing/LXCWP/master/files/default
+fi
 
 
 
